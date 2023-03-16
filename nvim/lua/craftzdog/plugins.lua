16810754775876961dfd96ea5ac3c9ef9035bb3e
wasuken <wevorence@gmail.com>
2023-03-17 01:34:51 +0900

    upd

--- nvim/lua/craftzdog/plugins.lua
+++ nvim/lua/craftzdog/plugins.lua
@@ -50,4 +50,8 @@ packer.startup(function(use)
 
   use 'lewis6991/gitsigns.nvim'
   use 'dinhhuy258/git.nvim' -- For git blame & browse
+  use 'roman/golden-ratio'
+  use 'neovim/nvim-lspconfig'
+  use 'MunifTanjim/prettier.nvim'
+  use("gbprod/yanky.nvim")
 end)

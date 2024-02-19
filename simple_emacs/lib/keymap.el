a01353958b2cf1cf2266aa7c081ebe3255669bce
wasuken <wasuken1@gmail.com>
2024-02-19 22:38:30 +0900

    added function simple-formatting.

--- simple_emacs/lib/keymap.el
+++ simple_emacs/lib/keymap.el
@@ -1,13 +1,14 @@
 ;; my tools keymap
-(global-set-key (kbd "C-c z c") 'tab-bar-new-tab)
-(global-set-key (kbd "C-c z n") 'tab-bar-switch-to-next-tab)
-(global-set-key (kbd "C-c z b") 'tab-bar-switch-to-prev-tab)
-(global-set-key (kbd "C->") 'other-window)
-(global-set-key (kbd "C-<") (lambda () (interactive) (other-window -1)))
 (global-set-key (kbd "C-c b i") 'insert-hugo-header)
-(global-set-key (kbd "C-c l") #'create-md-link)
+(global-set-key (kbd "C-c l") 'create-md-link)
 (global-set-key (kbd "C-x t s") 'toggle-window-split)
+(global-set-key (kbd "C-c M-d") 'generate-today-diary-file)
+(define-key markdown-mode-map (kbd "C-c RET") 'simple-formatting)
 
 ;; my keymap
 (global-set-key (kbd "C-c g") 'find-grep)
-(global-set-key (kbd "C-c M-d") 'generate-today-diary-file)
+(global-set-key (kbd "C-<") (lambda () (interactive) (other-window -1)))
+(global-set-key (kbd "C->") 'other-window)
+(global-set-key (kbd "C-c z c") 'tab-bar-new-tab)
+(global-set-key (kbd "C-c z n") 'tab-bar-switch-to-next-tab)
+(global-set-key (kbd "C-c z b") 'tab-bar-switch-to-prev-tab)

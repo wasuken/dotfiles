adfff541e6e79a654d42b9d46a3b6c97da9beebf
wasu <wasuken1@gmail.com>
2026-02-12 20:45:18 +0900

    fix

--- emacs/init.el
+++ emacs/init.el
@@ -32,16 +32,16 @@
 ;; Utils
 (load (expand-file-name "core/util.el" dotfiles-emacs-dir))
 
-;; Packages (requireでOK)
-(require 'completion)
-(require 'search)
-(require 'lsp)
-(require 'languages)
-(require 'ui)
-(require 'git)
-(require 'writing)
-(require 'ai)
-(require 'optional)
+;; Packages (全部load)
+(load (expand-file-name "packages/completion.el" dotfiles-emacs-dir))
+(load (expand-file-name "packages/search.el" dotfiles-emacs-dir))
+(load (expand-file-name "packages/lsp.el" dotfiles-emacs-dir))
+(load (expand-file-name "packages/languages.el" dotfiles-emacs-dir))
+(load (expand-file-name "packages/ui.el" dotfiles-emacs-dir))
+(load (expand-file-name "packages/git.el" dotfiles-emacs-dir))
+(load (expand-file-name "packages/writing.el" dotfiles-emacs-dir))
+(load (expand-file-name "packages/ai.el" dotfiles-emacs-dir))
+(load (expand-file-name "packages/optional.el" dotfiles-emacs-dir))
 
 ;; Font
 (let ((font-config (expand-file-name "core/font.el" dotfiles-emacs-dir)))
@@ -50,6 +50,11 @@
 
 ;; Keymap (最後にload)
 (load (expand-file-name "core/keymap.el" dotfiles-emacs-dir))
+
+(add-to-list 'load-path "~/dotfiles/emacs/mypackages/live-comments")
+(require 'live-comments)
+(setq live-comments-gemini-api-key gemini-api-key)
+;; (load (expand-file-name "mypackages/live-comments/live-comments.el" dotfiles-emacs-dir))
 
 (provide 'init)
 ;;; init.el ends here

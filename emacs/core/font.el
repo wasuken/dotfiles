01153584f1c3bc474200c887b7e80e88677b14a2
wasu <wasuken1@gmail.com>
2026-02-12 20:45:44 +0900

    fix

--- emacs/core/font.el
+++
@@ -1,9 +0,0 @@
-(set-face-attribute 'default nil :family "HackGen" :height 150)
-;; 絵文字用フォールバックを追加
-(set-fontset-font t 'symbol "Noto Color Emoji" nil 'prepend)
-(set-fontset-font t 'unicode "Noto Color Emoji" nil 'prepend)
-
-(provide 'font)
-;;; font.el ends here
-
-

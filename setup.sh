9531e7db07f35f212d7511c8cfc34c645eb313ee
wasu <wasuken1@gmail.com>
2026-07-11 09:07:49 +0900

    setup.sh修正
    現在の状態に合わせた

--- setup.sh
+++ setup.sh
@@ -6,9 +6,16 @@ touch ~/dotfiles/zsh/config.zsh
 
 # Neovim (if still using)
 mkdir -p ~/.config
+NVIM_DIR="$HOME/.config/nvim"
+if [ -d "$NVIM_DIR/.git" ]; then
+  git -C "$NVIM_DIR" pull
+else
+  git clone git@github.com:wasuken/nvim.git "$NVIM_DIR"
+fi
 
 # Emacs
-ln -sf ~/dotfiles/emacs ~/.emacs.d
+mkdir -p ~/.emacs.d
+ln -sf ~/dotfiles/emacs/init.el ~/.emacs.d/init.el
 
 # Create secret files
 touch ~/.emacs.d/config.el

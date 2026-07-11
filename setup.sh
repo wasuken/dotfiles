a1a65b221886bd4380361edef78097ba93aba7fd
wasu <wasuken1@gmail.com>
2026-07-11 09:02:46 +0900

    整理

--- setup.sh
+++ setup.sh
@@ -6,11 +6,10 @@ touch ~/dotfiles/zsh/config.zsh
 
 # Neovim (if still using)
 mkdir -p ~/.config
-ln -sf ~/dotfiles/nvim ~/.config/
 
 # Emacs
 ln -sf ~/dotfiles/emacs ~/.emacs.d
 
 # Create secret files
 touch ~/.emacs.d/config.el
-echo "# Add your secrets here (habitica-uid, gemini-api-key, etc)" >> ~/.emacs.d/config.el
+echo "# Add your secrets here (habitica-uid, gemini-api-key, etc)" >>~/.emacs.d/config.el
